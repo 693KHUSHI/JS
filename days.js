82a964b039dd6e6a7f8136e53fca594a75633218
Khushi <khushijain9548@gmail.com>
2024-07-17 15:59:20 +0530

    two files

---
+++ days.js
@@ -0,0 +1,27 @@
+let day=1;
+switch(day)
+{
+    case 1:
+        console.log("Its monday!(Bhaut ho gaya aram.Office to jana padega)");
+        break;
+        case 2:
+            console.log("Its tuesday!(Bhaut ho gaya aram.Office to jana padega)");
+            break;
+            case 3:
+                console.log("Its Wednesday!(Bhaut ho gaya aram.Office to jana padega)");
+                break;
+                case 4:
+                    console.log("Its Thursday!(Bhaut ho gaya aram.Office to jana padega)");
+                    break;
+                    case 5:
+                        console.log("Its Friday!(Bas ek din aur)");
+                        break;
+                        case 6:
+                            console.log("Its Saturady!(Time for clubbing )");
+                            break;
+                            case 7:
+                                console.log("Its sunday!(Have funday)");
+                                break;
+                                default:
+                                    console.log("Kundli he kharab h");
+}
---
+++ trafficlight.js
@@ -0,0 +1,16 @@
+let a='red';
+switch(a)
+{
+    case "red":
+        console.log("Stop");
+        break;
+        case "yellow":
+            console.log("Ready");
+            break;
+            case "green":
+                console.log("Go");
+                break;
+                default:
+                    console.log("Light is broken");
+
+}
